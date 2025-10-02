0a126a224097ecbde998140bdc8b1ce2d841d1dd
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-02 22:31:28 +0300

    Migrate from JDBCTemplate to Spring Data JPA

---
+++ example_35/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.eazybytes.eazyschool.config;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    /* /home - /holidays - /contact - /saveMsg - /about - /courses */
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf( csrf -> csrf.ignoringRequestMatchers("/saveMsg") )
+                .authorizeHttpRequests( (requests) -> requests.requestMatchers("/" , "/home").permitAll()
+                        .requestMatchers("/dashboard").authenticated()
+                        .requestMatchers("/displayMessages").hasRole("ADMIN")
+                        .requestMatchers("/closeMsg/**").hasRole("ADMIN")
+                        .requestMatchers("/holidays/**").permitAll()
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/saveMsg").permitAll()
+                        .requestMatchers("/courses").permitAll()
+                        .requestMatchers("/about").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/logout").permitAll())
+                .formLogin( flc -> flc.loginPage("/login")
+                        .defaultSuccessUrl("/dashboard")
+                        .failureUrl("/login?error=true").permitAll())
+                .logout( lc -> lc.logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("12345").roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("54321").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+
+}
