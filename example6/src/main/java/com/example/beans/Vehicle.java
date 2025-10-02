e8f96bd6df3ba9cc88687c9fa9d01d49cbbeacb4
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-02 18:12:08 +0300

    PostConstruct & PreDestroy Annotations

---
+++ example6/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,32 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @PostConstruct
+    public void initialize(){
+        this.setName("Honda");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Destroy Vehicle Bean");
+    }
+
+    public void printHello(){
+        System.out.println("Printing hello from Component Vehicle Bean");
+    }
+}
