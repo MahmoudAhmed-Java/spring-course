0ba911d9f112d98b01161d0534407f7c1e73a1fb
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-02 18:17:10 +0300

    Wiring Beans using method parameters

---
+++ example_10/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,29 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+public class Vehicle {
+    private String name;
+
+    public Vehicle(){
+        System.out.println("Vehicle bean created by Spring");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println("Printing hello from Component Vehicle Bean");
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle name is - "+this.name;
+    }
+}
