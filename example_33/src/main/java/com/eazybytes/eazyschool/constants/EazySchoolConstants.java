a3e702d52a536a8befb9223e7c25da33af42c695
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-02 22:16:58 +0300

    Display Holidays from H2 DB using BeanPropertyRowMapper

---
+++ example_33/src/main/java/com/eazybytes/eazyschool/constants/EazySchoolConstants.java
@@ -0,0 +1,7 @@
+package com.eazybytes.eazyschool.constants;
+
+public class EazySchoolConstants {
+    public static final String ANONYMOUS = "anonymous";
+    public static final String OPEN = "open";
+    public static final String CLOSE = "close";
+}
