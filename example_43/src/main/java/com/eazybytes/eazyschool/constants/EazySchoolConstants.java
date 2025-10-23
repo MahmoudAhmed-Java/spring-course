be48782a55dc3707ec9bea1462329c2542f71601
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-23 13:40:28 +0300

    Sorting, Pagination using Spring Data JPA

---
+++ example_43/src/main/java/com/eazybytes/eazyschool/constants/EazySchoolConstants.java
@@ -0,0 +1,9 @@
+package com.eazybytes.eazyschool.constants;
+
+public class EazySchoolConstants {
+    //public static final String ANONYMOUS = "anonymous";
+    public static final String OPEN = "open";
+    public static final String CLOSE = "close";
+    public static final String STUDENT_ROLE = "STUDENT";
+    public static final String ADMIN_ROLE = "ADMIN";
+}
---
+++ example_43/src/main/resources/eazyschool_schema.sql
@@ -0,0 +1,61 @@
+create database eazyschool;
+
+user eazyschool;
+
+CREATE TABLE IF NOT EXISTS `contact_msg` (
+  `contact_id` int AUTO_INCREMENT  PRIMARY KEY,
+  `name` varchar(100) NOT NULL,
+  `mobile_num` varchar(10) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `message` varchar(500) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `holidays` (
+  `day` varchar(20) NOT NULL,
+  `reason` varchar(100) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `class` (
+	`class_id` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(100) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+	`created_by` varchar(50) NOT NULL,
+	`updated_at` TIMESTAMP DEFAULT NULL,
+	`updated_by` varchar(50) DEFAULT NULL,
+	PRIMARY KEY (`class_id`)
+);
+
+ALTER TABLE `person`
+ADD COLUMN `class_id` int null AFTER `address_id`,
+ADD CONSTRAINT `FK_CLASS_CLASS_ID` FOREIGN KEY (class_id)
+REFERENCES class(class_id);
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  `course_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `fees` varchar(10) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL,
+   PRIMARY KEY (`course_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `person_courses` (
+  `person_id` int NOT NULL,
+  `course_id` int NOT NULL,
+  FOREIGN KEY (person_id) REFERENCES person(person_id),
+  FOREIGN KEY (course_id) REFERENCES courses(course_id),
+   PRIMARY KEY (`person_id`,`course_id`)
+);
