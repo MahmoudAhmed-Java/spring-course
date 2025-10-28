af1ca3a8b7772a296657f01810f43f4b2c1db8e1
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-28 19:05:18 +0300

    Spring Data Rest & HAL Explorer changes

---
+++ example_47/src/main/java/com/eazybytes/eazyschool/model/EazyClass.java
@@ -0,0 +1,27 @@
+package com.eazybytes.eazyschool.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@Entity
+@Table( name = "class")
+public class EazyClass extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int classId;
+
+    @NotBlank(message = "Name must be not blank")
+    @Size(min=3 , message = "Name must be al least 3 characters long")
+    private String name;
+
+    @OneToMany(mappedBy = "eazyClass" , fetch = FetchType.LAZY , cascade = CascadeType.PERSIST , targetEntity = Person.class)
+    private Set<Person> persons;
+}
